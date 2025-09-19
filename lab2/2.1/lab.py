2252c452c2b3ddd9d99ade3813164d403494fc6e
Mykhailo Buhera <mihapvp1990@gmail.com>
2025-09-20 00:32:08 +0300

    made sum of odd

---
+++ lab2/2.1/lab.py
@@ -0,0 +1,11 @@
+from array import *
+
+def main():
+    arr = array('i', [1, 2, 3, 4, -5])
+    sum_neparn = 0
+    for val in arr:
+        if val % 2 != 0:
+            sum_neparn += val
+    print("Sum of odd elements:", sum_neparn)
+
+main()
