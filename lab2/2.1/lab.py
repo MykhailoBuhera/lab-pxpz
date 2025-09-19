35f1beaffa938562f21389a018f221d629a8cfb3
Mykhailo Buhera <mihapvp1990@gmail.com>
2025-09-20 00:44:42 +0300

    end with replace elements which % 3 ==0

--- lab2/2.1/lab.py
+++ lab2/2.1/lab.py
@@ -7,5 +7,11 @@ def main():
         if val % 2 != 0 and val < 0:
             sum_neparn += val
     print("Sum of odd elements:", sum_neparn)
+    for i, val in enumerate(arr):
+        if val %3 == 0:
+            arr[i] = sum_neparn
+            print("Modified array:")
+            for v in arr:
+                print(v)
 
 main()
