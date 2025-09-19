db0b65c1675ec5c6f525a02235e4e8024d357530
Mykhailo Buhera <mihapvp1990@gmail.com>
2025-09-20 00:36:46 +0300

    end wit first task from lab

--- lab2/2.1/lab.py
+++ lab2/2.1/lab.py
@@ -1,10 +1,10 @@
 from array import *
 
 def main():
-    arr = array('i', [1, 2, 3, 4, -5])
+    arr = array('i', [-1, 2, 3, 4, -5])
     sum_neparn = 0
     for val in arr:
-        if val % 2 != 0:
+        if val % 2 != 0 and val < 0:
             sum_neparn += val
     print("Sum of odd elements:", sum_neparn)
 
