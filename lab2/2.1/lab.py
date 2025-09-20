a2907ac5c95c70bec9aec735b3c7034f96cb82c3
Mykhailo Buhera <mihapvp1990@gmail.com>
2025-09-20 13:00:51 +0300

    totale compared lab 2.1 to damand

--- lab2/2.1/lab.py
+++ lab2/2.1/lab.py
@@ -1,12 +1,24 @@
 from array import *
 
-def main():
-    arr = array('i', [-1, 2, 3, 4, -5])
+def arraye():
+    arr = array('i', [])
+    size = int(input("Enter the size of the array: "))
+    if size <= 0:
+        print("Size must be a positive integer.")
+        return
+    for _ in range(size):
+        znach = int(input("Enter the value to fill the array: "))
+        arr.append(znach)
+    nearni(arr)
+
+def nearni (arr):                
     sum_neparn = 0
     for val in arr:
         if val % 2 != 0 and val < 0:
             sum_neparn += val
     print("Sum of odd elements:", sum_neparn)
+    modif(arr, sum_neparn)
+def modif (arr, sum_neparn):    
     for i, val in enumerate(arr):
         if val %3 == 0:
             arr[i] = sum_neparn
@@ -14,4 +26,5 @@ def main():
             for v in arr:
                 print(v)
 
-main()
+arraye()
+    
