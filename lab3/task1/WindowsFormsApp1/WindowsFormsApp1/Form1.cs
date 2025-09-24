8e159c4d12298dfed39e626446d45630c5416a63
Mykhailo Buhera <mihapvp1990@gmail.com>
2025-09-24 16:13:51 +0300

    end with task 1

---
+++ lab3/task1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            char [] arr = input.ToCharArray();
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 'r' || arr[i] == 'k' || arr[i] == 't')
+                {
+                   count++;
+                }
+            }
+            label2.Text = "Kilkist bykv r,k,t = " + count;
+        }
+    }
+}
